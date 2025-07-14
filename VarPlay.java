c40d0f413160cd2e39a88f283825a8ba4fe51685
chandra0013 <126132528+chandra0013@users.noreply.github.com>
2025-07-15 02:56:48 +0530

    Create VarPlay.java

---
+++ VarPlay.java
@@ -0,0 +1,14 @@
+public class VarPlay{
+  public static void main(String[] args){
+    int amma=21;
+    String nana="hi nana";
+    boolean sheshi=true;
+    char anish='A';
+    float smitha=9.91f;
+    System.out.println(amma+smitha);
+    System.out.println(nana + "bagunnava");
+    System.out.println(sheshi);
+    System.out.println(anish+"nish");
+    System.out.println(smitha*10);
+  }
+}
